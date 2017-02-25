74dc5d04c4005d93df1ca758f6d3f4eee1e91fc0
amy61789 <amy61789@outlook.com>
2017-02-25 15:37:50 -0800

    Category Database

---
+++ category.sql
@@ -0,0 +1,4 @@
+INSERT INTO `NA_Categories` (`CategoryID`, `Category`, `Description`) VALUES
+(1,	'Technology',	'Technology is important in today\'s world because it serves a variety of functions in many of the most important aspects of modern society, like education, communication, business and scientific progress.'),
+(2,	'Food',	'Keep up with the latest recipes, restaurants, and chefs.'),
+(3,	'Movies',	'New movie releases available.');
