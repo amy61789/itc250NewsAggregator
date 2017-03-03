6c1bd613f601a24b8168aba3d841972a11ef3156
amy61789 <amy61789@outlook.com>
2017-03-02 17:19:00 -0800

    Updated files

--- category_view.php
+++ category_view.php
@@ -22,7 +22,7 @@ if(isset($_GET['id']) && (int)$_GET['id'] > 0){#proper data must be on querystri
 }
 
 //sql statement to select individual item
-$sql = "select FeedID,FeedName,Description from NA_Feed where CategoryID = " . $myID;
+$sql = "select FeedID,FeedName,Description,URL from NA_Feed where CategoryID = " . $myID;
 //---end config area --------------------------------------------------
 
 $foundRecord = FALSE; # Will change to true, if record found!
@@ -37,8 +37,8 @@ if(mysqli_num_rows($result) > 0)
 	   while ($row = mysqli_fetch_assoc($result))
 	   {
         echo '<div align="center"><a href="' . VIRTUAL_PATH . 'category/category_view.php?id=' . (int)$row['FeedID'] . '">' . dbOut($row['FeedName']) . '</a><br/>';
-        echo '<tr><td>Description: ' . $row['Description'] . '</td></tr> ';
-	echo '<tr><td>URL: ' . $row['URL'] . '</td></tr> ';
+        echo '<tr><td>Description: ' . $row['Description'] . '</td></tr><br> ';
+	echo '<tr><td>URL: <br>' . $row['URL'] . '</td></tr> ';
 	   }
 }
 
--- index.php
+++ index.php
@@ -27,11 +27,11 @@ require '../inc_0700/config_inc.php'; #provides configuration, pathing, error ha
 $sql = "select * from NA_Categories";
 
 #Fills <title> tag. If left empty will default to $PageTitle in config_inc.php  
-$config->titleTag = 'Surveys made with love & PHP in Seattle';
+$config->titleTag = 'Categories';
 
 #Fills <meta> tags.  Currently we're adding to the existing meta tags in config_inc.php
-$config->metaDescription = 'Seattle Central\'s ITC280 Class Surveys are made with pure PHP! ' . $config->metaDescription;
-$config->metaKeywords = 'Surveys,PHP,Fun,Bran,Regular,Regular Expressions,'. $config->metaKeywords;
+//$config->metaDescription = 'Seattle Central\'s ITC280 Class Surveys are made with pure PHP! ' . $config->metaDescription;
+//$config->metaKeywords = 'Surveys,PHP,Fun,Bran,Regular,Regular Expressions,'. $config->metaKeywords;
 
 /*
 $config->metaDescription = 'Web Database ITC281 class website.'; #Fills <meta> tags.
@@ -53,36 +53,18 @@ get_header(); #defaults to theme header or header_inc.php
 <h3 align="center"><?=smartTitle();?></h3>
  
 <?php
-#reference images for pager
-$prev = '<img src="' . VIRTUAL_PATH . 'images/arrow_prev.gif" border="0" />';
-$next = '<img src="' . VIRTUAL_PATH . 'images/arrow_next.gif" border="0" />';
-
-# Create instance of new 'pager' class
-$myPager = new Pager(3,'',$prev,$next,'');
-$sql = $myPager->loadSQL($sql);  #load SQL, add offset
 
 # connection comes first in mysqli (improved) function
 $result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
 
 if(mysqli_num_rows($result) > 0)
 {#records exist - process
-	if($myPager->showTotal()==1){$itemz = "category";}else{$itemz = "categories";}  //deal with plural
-    echo '<div align="center">We have ' . $myPager->showTotal() . ' ' . $itemz . '!</div>';
 	while($row = mysqli_fetch_assoc($result))
 	{# process each row
-          
-         echo '<table class="table table-striped table-hover">
-          <thead>
-           <tr>
-            <th>Category</th>
-           </tr>
-          </thead>
-          <tbody>
-           <tr>';
-     echo '<td>Title: <a href="' . VIRTUAL_PATH . 'category/category_view.php?id=' . (int)$row['CategoryID'] . '">' . dbOut($row['Category']) . '</a></td></tr>';
-     echo '<tr><td>Description: ' . $row['Description'] . '</td></tr> ';
-	}
-	echo $myPager->showNAV(); # show paging nav, only if enough records	 
+         echo '<div align="center"><a href="' . VIRTUAL_PATH . 'category/category_view.php?id=' . (int)$row['CategoryID'] . '">' . dbOut($row['Category']) . '</a><br>';
+         echo '<tr><td>Description: ' . $row['Description'] . '</td></tr> ';
+ 
+	} 
 }else{#no records
     echo "<div align=center>What! No categories?  There must be a mistake!!</div>";	
 }
